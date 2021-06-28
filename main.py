40207eddc5a04493b40f567980f708bac2fef0b9
theogcrafted <77366643+theogcrafted@users.noreply.github.com>
2021-06-28 14:52:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import discord
 ## Our bot 
 client = discord.Client()
 
+@client.event
+
+async def on_ready():
 ##Things to do 
+    general_channel = client.get_channel(855657973438611467)
+
+    await general_channel.send('Hello, World!')
 
 ## Run the client 
-client.run()
+client.run('ODU3Njg5OTQyNjkwODg5NzM4.YNTQAQ.E3s4n8zRuuV6vGw5CZppN5DcfyM')
