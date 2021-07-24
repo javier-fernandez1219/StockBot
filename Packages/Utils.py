54000cea230d54f86c70081a8c46f4d808f6c93a
Daniel Rodriguez <daniel@Daniels-MBP-2.attlocal.net>
2021-07-24 13:10:25 -0400

    Use yFi instead to get latest quotes. Add .gitignore.

--- Packages/Utils.py
+++ Packages/Utils.py
@@ -20,23 +20,46 @@ class Finance():
         return d.dividends.tail(1)
     
     def get_userlist(self, user):
-        return(self.userlist[user])
+        result = {}
+        for ticker in self.userlist[user].keys():
+            qoute = self.get_last_quote(ticker)
+            result[self.userlist[user][ticker]] = f'({qoute["ticker"]})     {qoute["currentPrice"]}     {qoute["changeDollar"]} ({qoute["changePercent"]}%)'
+        return(result)
 
     def add_userlist_item(self, user, ticker):
+        t = yf.Ticker(ticker)
+        name = t.info['shortName']
         if user in self.userlist.keys():
             list = self.userlist[user]
-            if ticker in list:
+            if ticker in list.keys():
                 return("Ticker already added!")
             else:
-                list.append(self.getData(ticker))
+                list[ticker] = name
         else:
-            self.userlist[user]=[self.getData(ticker)]
+            self.userlist[user] = {ticker: name}
         return "Ticker added to your watchlist!"
 
     def del_userlist_item(self, user, ticker):
-        l = self.userlist[user]
-        self.userlist[user] = list(filter(lambda i: i['symbol'] != ticker.upper(), l))            
+        self.userlist[user].remove(ticker)
+        #self.userlist[user] = list(filter(lambda i: i['symbol'] != ticker.upper(), l))            
         return "Ticker removed from the list!"
+    def get_last_quote(self, ticker):
+        t = yf.Ticker(ticker)
+        data = t.history(period="max", interval="1d").tail(2)
+        beforeLastDay = data.iloc[0, :]
+        lastDay = data.iloc[1, :]
+        #last_quote = data.tail(1)
+        closePrice = round(lastDay['Close'],3)
+        previousClose = round(beforeLastDay['Close'],3)
+        changeDollar = round(closePrice - previousClose,3)
+        changePercent = round((changeDollar/previousClose)*100,3)
+        qoute = {
+            "ticker" : ticker,
+            "currentPrice" : closePrice,
+            "changeDollar" : changeDollar,
+            "changePercent" : changePercent
+        }
+        return qoute
 
     def getData(self, symbol):
         url=f'https://finance.yahoo.com/quote/{symbol.upper()}'
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith('quote!'):
+    if message.content.lower().startswith('q!'):
         t = message.content.split(":")[1]
         resp = yfi.get_ticker(ticker=t)
         await message.channel.send(resp)
@@ -36,10 +36,8 @@ async def on_message(message):
         userid = message.author.id
         ment = message.author.mention
         embedVar = discord.Embed(title=f'Watch List', description=f"Stocks {ment} likes!", color=0x00ff00)
-        i = 1
-        for t in user_list:
-            embedVar.add_field(name=f'Stock{i}:', value=t, inline=False)
-            i+=1
+        for t in user_list.keys():
+            embedVar.add_field(name=t, value=user_list[t], inline=False)
         embedVar.set_footer(text="Cheers!!")
         await message.channel.send(embed=embedVar)
       
